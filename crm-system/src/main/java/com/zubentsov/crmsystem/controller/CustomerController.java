4102c38d257a5ad673639f1667d42d94c2a5628c
vitaly <vitaly.zubentsov@ya.ru>
2021-12-08 22:47:14 +0300

    test spring mvc controller

---
+++ crm-system/src/main/java/com/zubentsov/crmsystem/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package com.zubentsov.crmsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@RequestMapping("/list")
+	public String listCustomer(Model model) {
+		
+		return "list-customers";
+	}
+
+}
