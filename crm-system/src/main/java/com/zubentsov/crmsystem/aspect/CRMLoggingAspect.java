b7aebc3a901ebfa5db87b360df9503b6afa5674b
vitaly <vitaly.zubentsov@ya.ru>
2021-12-22 22:15:44 +0300

    add logging app flow with @After advice

--- crm-system/src/main/java/com/zubentsov/crmsystem/aspect/CRMLoggingAspect.java
+++ crm-system/src/main/java/com/zubentsov/crmsystem/aspect/CRMLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.zubentsov.crmsystem.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -26,30 +27,40 @@ public class CRMLoggingAspect {
 	@Pointcut("execution(* com.zubentsov.crmsystem.service.*.*(..))")
 	private void forServicePackage() {
 	}
-	
+
 	@Pointcut("forDaoPackage() || forControllerPackage() || forServicePackage()")
 	private void forAppFlow() {
 	}
 
 	// add @Before advice
-	
+
 	@Before("forAppFlow()")
 	public void before(JoinPoint joinPoint) {
-		
-		//display calling method
-		
+
+		// display calling method
+
 		logger.info("======>> in @Before : " + joinPoint.getSignature().toShortString());
-		
-		//display the arguments of method
-		
+
+		// display the arguments of method
+
 		Object[] args = joinPoint.getArgs();
-		
-		for (Object arg: args) {
+
+		for (Object arg : args) {
 			logger.info("======>> argument : " + arg.toString());
 		}
-		
-		
 	}
 
+	@AfterReturning(pointcut = "forAppFlow()", returning = "result")
+	public void after(JoinPoint joinPoint, Object result) {
+
+		// display calling method
+
+		logger.info("======>> in @After : " + joinPoint.getSignature().toShortString());
+
+		// display the arguments of method
+
+		logger.info("======>> argument : " + result);
+
+	}
 
 }
