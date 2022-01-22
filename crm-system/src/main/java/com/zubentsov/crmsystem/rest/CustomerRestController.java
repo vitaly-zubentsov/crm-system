deeb5b17b2dce3381b80823225888d1129993aa7
vitaly <vitaly.zubentsov@ya.ru>
2022-01-22 21:48:46 +0300

    add rest api for update customer

--- crm-system/src/main/java/com/zubentsov/crmsystem/rest/CustomerRestController.java
+++ crm-system/src/main/java/com/zubentsov/crmsystem/rest/CustomerRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +50,13 @@ public class CustomerRestController {
 		
 		customerService.saveOrUpdateCustomer(customer);
 		
+		return customer;
+	}
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+	
+		customerService.saveOrUpdateCustomer(customer); 	
 		return customer;
 	}
 	
