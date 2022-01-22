b7c95faa668ea45146b371cf83c6654ec0bc12b9
vitaly <vitaly.zubentsov@ya.ru>
2022-01-22 20:54:51 +0300

    add exception handling in rest api

---
+++ crm-system/src/main/java/com/zubentsov/crmsystem/rest/CustomerNotFoundException.java
@@ -0,0 +1,27 @@
+package com.zubentsov.crmsystem.rest;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID =  1L;
+
+	public CustomerNotFoundException() {
+	}
+
+	public CustomerNotFoundException(String message) {
+		super(message);
+	}
+
+	public CustomerNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+	public CustomerNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CustomerNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
--- crm-system/src/main/java/com/zubentsov/crmsystem/rest/CustomerRestController.java
+++ crm-system/src/main/java/com/zubentsov/crmsystem/rest/CustomerRestController.java
@@ -3,6 +3,9 @@ package com.zubentsov.crmsystem.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +31,33 @@ public class CustomerRestController {
 	public Customer getCustomer(@PathVariable int customerId) throws Exception {
 		
 		Customer customer = customerServise.getCustomer(customerId);
+		
+		if (customer == null) {
+			throw new CustomerNotFoundException("Customer id not found: " + customerId);
+		}
 		return customer;
 	}
+	
+	
+	
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException exc) {
+		
+		CustomerErrorResponse error = new CustomerErrorResponse(
+				HttpStatus.NOT_FOUND.value(), exc.getMessage(), System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception exc) {
+		
+		CustomerErrorResponse error = new CustomerErrorResponse(
+				HttpStatus.BAD_REQUEST .value(), exc.getMessage(), System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		
+	}
+	
 }
